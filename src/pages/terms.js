74a5ee6417cce2c46d86013b3aa0bf095c80fc53
avergnaud <a.vergnaud@gmail.com>
2020-09-30 17:53:30 +0200

    pour test

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -39,7 +39,7 @@ export default function About() {
               <p>
                 En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004
                 pour la confiance dans l'économie numérique, il est précisé aux
-                utilisateurs du site internet https://cat-fintech.netlify.app/
+                utilisateurs du site https://cat-fintech.netlify.app/
                 l'identité des différents intervenants dans le cadre de sa
                 réalisation et de son suivi : 
                 <ul className="list-group list-group-flush">
